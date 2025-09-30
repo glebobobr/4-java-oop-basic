5e8e997c32a5e663faca8c89351ffc653c2224a2
glebobobr <glebvoronin456@gmail.com>
2025-09-30 07:38:07 +0500

    Воронин Глеб. Задание 05

--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -2,6 +2,20 @@ package com.example.task05;
 
 public class Task05Main {
     public static void main(String[] args) {
+        PolygonalLine line = new PolygonalLine();
 
+        line.addPoint(new Point(1, 2));
+        line.addPoint(4, 6);
+        line.addPoint(new Point(8, 9));
+
+        System.out.println("Длина ломаной: " + line.getLength());
+
+        Point[] points = {
+                new Point(0, 0),
+                new Point(3, 4),
+                new Point(6, 8)
+        };
+        line.setPoints(points);
+        System.out.println("Новая длина ломаной: " + line.getLength());
     }
 }
