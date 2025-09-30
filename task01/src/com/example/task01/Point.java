b7669757925bd6277e9bc401ee98caa270d06e4e
glebobobr <glebvoronin456@gmail.com>
2025-09-30 07:37:08 +0500

    Воронин Глеб. Задание 01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,8 +7,34 @@ public class Point {
     int x;
     int y;
 
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int temp = x;
+        x = -y;
+        y = -temp;
+    }
+
+    double distance(Point point) {
+        int dx = this.x - point.x;
+        int dy = this.y - point.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
-        String pointToString = String.format("(%d, %d)", x, y);
-        System.out.println(pointToString);
+        System.out.println(toString());
     }
 }
